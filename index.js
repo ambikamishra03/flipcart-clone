dcf2fcf10b66a93de15ab864a0cf6e0a5fe29ab1
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2023-09-28 19:55:57 +0530

    commits

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/', Routes);
 // ----------------production -----------------
 if (process.env.NODE_ENV === 'production') 
  {
-    //*Set static folder up in production
+    //Set static folder up in production
     app.use(express.static('frontend/build'));
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build','index.html')));
